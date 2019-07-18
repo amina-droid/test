c59f7cc5f11a1bd82f95300302b617a4ba125653
amina-droid <amina.rabadanova42@gmail.com>
2019-07-18 18:14:54 +0500

    js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,8 +1,14 @@
-var but_signIn = document.querySelector('.but_sign-in')
-
-but_signIn.addEventListener('click', function(){
-    console.log('fff')
-    // Ты пирожок <3
+// Ты пирожок <3
     //я тебя люблю ^-^
     // Я тоже тебя люблю :3
-})
+var but_signIn = document.querySelector('.but_sign-in')
+var email = document.querySelector('#e-m')
+let name = document.querySelector('.name')
+
+console.log(name)
+console.log(document.querySelector('.name'))
+function singIn(){
+    name.innerText = email.value
+    console.log(name)
+}
+but_signIn.addEventListener('click', singIn)
